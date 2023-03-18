5878fea8191bb5383f9eb5460e7d41d52694d148
wusize <wusize@sensetime.com>
2023-03-18 17:14:56 +0800

    'ms'

--- configs/soco/mask_rcnn_r50_fpn_syncbn_90k_coco.py
+++ configs/soco/mask_rcnn_r50_fpn_syncbn_90k_coco.py
@@ -29,6 +29,23 @@ optim_wrapper = dict(
     clip_grad=dict(max_norm=35, norm_type=2),
 )
 load_from = 'checkpoints/res50_fpn_soco_star_400.pth'
+
+
+# dataset settings
+file_client_args = dict(backend='disk')
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='RandomChoiceResize',
+        scales=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                (1333, 768), (1333, 800)],
+        keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
 train_dataloader = dict(
-    sampler=dict(type='InfiniteSampler'),
+    sampler=dict(type='InfiniteSampler', shuffle=True),
+    dataset=dict(
+        pipeline=train_pipeline)
 )
