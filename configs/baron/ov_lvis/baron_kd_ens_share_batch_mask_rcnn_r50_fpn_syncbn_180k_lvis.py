a9bf5924d52d0f96f1860f56393cd13d6408568c
wusize <wusize@sensetime.com>
2023-03-18 18:06:23 +0800

    'lvis_share_batch_kd'

---
+++ configs/baron/ov_lvis/baron_kd_ens_share_batch_mask_rcnn_r50_fpn_syncbn_180k_lvis.py
@@ -0,0 +1,12 @@
+_base_ = './baron_kd_share_batch_mask_rcnn_r50_fpn_syncbn_180k_lvis.py'
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(
+            type='EnsembleBaronShared4Conv1FCBBoxHead',
+            ensemble_factor=2.0 / 3.0,
+            class_info='data/metadata/lvis_v1_train_cat_norare_info.json',
+            transfer_factor=None
+        ),
+    ),
+)
